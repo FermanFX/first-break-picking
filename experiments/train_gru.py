8eec9ef3fce2f8a923bf70ab1d9fa231327fdc16
Fərman Xankişiyev <146029194+FermanFX@users.noreply.github.com>
2026-08-20 08:39:52 +0400

    Clean up imports in train_gru.py
    Removed unnecessary path setup for ROOT.

--- experiments/train_gru.py
+++ experiments/train_gru.py
@@ -4,11 +4,6 @@ import numpy as np
 import torch
 from torch import nn
 from tqdm import tqdm
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[1]
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))
-
 from src.data_loader import create_dataloader
 from src.loss import FirstBreakLoss
 from src.model import FirstBreakGRU
